ff94454ebb4a6075ae3317636861fb4120e32d9c
Charlie91 <aleksand44@yandex.ru>
2020-02-02 18:53:16 +0300

    add 1 task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,8 +1,38 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
+const filesDirectory = '/files/';
+
+function handleError(err, res) {
+  if (err.code === 'ENOENT') {
+    res.statusCode = 404;
+    res.end();
+    return;
+  }
+  res.statusCode = 500;
+  res.end();
+}
+
+function getFile(pathname, res) {
+  if (~pathname.indexOf('/')) {
+    res.statusCode = 400;
+    res.end();
+    return;
+  }
+
+  const filePath = path.join(__dirname, filesDirectory, pathname);
+
+  const readStream = fs.createReadStream(filePath);
+
+  readStream.on('error', (err) => handleError(err,res));
+
+  readStream
+      .pipe(res)
+      .on('error', (err) => handleError(err,res));
+}
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
@@ -11,7 +41,7 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
-
+      getFile(pathname, res);
       break;
 
     default:
