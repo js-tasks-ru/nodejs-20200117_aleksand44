13684b6638fa0a272dcc475a99ab7607f64ebd6c
Charlie91 <aleksand44@yandex.ru>
2020-02-03 23:02:24 +0300

    add handler of file creation

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,17 +1,54 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+function createFile(req, res) {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+  let body = [];
 
+  if (~pathname.indexOf('/')) {
+    res.statusCode = 400;
+    res.end('вложенные пути запрещены');
+    return;
+  }
+
+  req
+      .on('data', (data) => body.push(data))
+      .on('end', () => {
+        body = Buffer.concat(body).toString();
+
+        const writableStream = fs.createWriteStream(filepath, {flags: 'wx'});
+
+        writableStream.on('error', (err) => {
+          console.log('err', err);
+          if (err.code === 'EEXIST') {
+            res.statusCode = 409;
+            res.end('данный фыйл уже существует');
+            return;
+          }
+          fs.unlink(filepath);
+          res.statusCode = 500;
+          res.end('серверная ошибка');
+        });
+        writableStream.on('finish', () => {
+          res.statusCode = 201;
+          res.end('alright');
+        });
+
+        writableStream.write(body);
+        writableStream.end();
+      });
+}
+
+server.on('request', (req, res) => {
   switch (req.method) {
     case 'POST':
-
+      createFile(req, res);
       break;
 
     default:
