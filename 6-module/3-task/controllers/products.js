fefcb45af09ad3ba22bab9b1d23b61ed0dd6c10e
Charlie91 <aleksand44@yandex.ru>
2020-02-16 12:06:51 +0300

    add 3 task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,12 @@
+const Product = require('./../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const { query: {query} } = ctx.request;
+
+  const results = await Product
+    .find({$text: { $search: query }}, { score: { $meta: 'textScore' } })
+    .sort({ score: { $meta: 'textScore' } })
+    .limit(20);
+
+  ctx.body = {products: results};
 };
