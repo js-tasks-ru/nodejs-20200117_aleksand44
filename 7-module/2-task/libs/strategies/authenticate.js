1c0c898fbc24be642d169c11812791db17040afd
Charlie91 <aleksand44@yandex.ru>
2020-02-16 16:51:37 +0300

    quick fix

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,36 +1,23 @@
 const User = require('../../models/User');
 
-function ValidationError(message) {
-  this.name = 'ValidationError';
-  this.message = (message || '');
-  this.errors = {
-    email: {
-      message: 'Некорректный email.',
-    },
-  };
-}
-ValidationError.prototype = Error.prototype;
-
 module.exports = async function authenticate(strategy, email, displayName, done) {
   if (!email) {
     return done(null, false, 'Не указан email');
   }
 
-  if (!~email.indexOf('@')) {
-    const err = new ValidationError({
-      email,
-    });
-    console.log(err.name);
-    return done(err);
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const newUser = await User.create({ email, displayName });
+
+      return done(null, newUser);
+    }
+    return done(null, user);
   }
 
-  const user = await User.findOne({ email });
-
-  if (!user) {
-    const newUser = await User.create({ email, displayName });
-
-    return done(null, newUser);
+  catch (e) {
+    done(e);
   }
 
-  return done(null, user);
 };
