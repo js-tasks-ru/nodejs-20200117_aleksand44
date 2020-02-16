bc10566220ed0c0c2fcd6e599bf7efde3fa5a683
Charlie91 <aleksand44@yandex.ru>
2020-02-16 16:47:26 +0300

    add 2 task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,36 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+function ValidationError(message) {
+  this.name = 'ValidationError';
+  this.message = (message || '');
+  this.errors = {
+    email: {
+      message: 'Некорректный email.',
+    },
+  };
+}
+ValidationError.prototype = Error.prototype;
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  }
+
+  if (!~email.indexOf('@')) {
+    const err = new ValidationError({
+      email,
+    });
+    console.log(err.name);
+    return done(err);
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    const newUser = await User.create({ email, displayName });
+
+    return done(null, newUser);
+  }
+
+  return done(null, user);
 };
