c343a08a93e5e7140f72db7e368ec56932d433ce
Charlie91 <aleksand44@yandex.ru>
2020-02-25 01:10:20 +0300

    add 9-module, first task

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -7,11 +7,36 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
-    next();
+    const {token} = socket.handshake.query;
+
+    if (!token) {
+      return next(new Error('anonymous sessions are not allowed'));
+    }
+    const session = await Session.findOne({token}).populate('user');
+
+    if (!session) {
+      return next(new Error('wrong or expired session token'));
+    }
+
+    const {user: {email, displayName, _id: id}} = session;
+
+    socket.user = {
+      email,
+      displayName,
+      id,
+    };
+    return next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      await Message.create({
+        user: socket.user.displayName,
+        chat: socket.user.id,
+        date: new Date(),
+        text: msg,
+      });
+    });
   });
 
   return io;
